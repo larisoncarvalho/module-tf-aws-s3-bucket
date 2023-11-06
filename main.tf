181353d84180d952ff01a21c3e3d9b40b0927391
Johannes Scheuerer <joscheuerer@gmail.com>
2023-11-06 13:30:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_logging" "this" {
   count = local.create_bucket && length(keys(var.logging)) > 0 ? 1 : 0
 
-  bucket = aws_s3_bucket.this[0].id
+  bucket = "jo-logging" #aws_s3_bucket.this[0].id
 
   target_bucket = var.logging["target_bucket"]
   target_prefix = try(var.logging["target_prefix"], null)
